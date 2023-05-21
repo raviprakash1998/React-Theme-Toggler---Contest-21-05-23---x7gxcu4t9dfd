63432045e4b055bda00cf0582564df65cefb38da
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-05-21 14:15:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const [isDarkMode, setIsDarkMode] = useState();
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
   return (
-    <div id="main">
-      <h1></h1>
-      <button></button>
+    <div id="main" style={{ background: isDarkMode ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)', color: isDarkMode ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)' }}>
+      <h1>{isDarkMode ? 'Dark Mode Activated' : 'Light Mode Activated'}</h1>
+      <button onClick={toggleTheme}>{isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}</button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
